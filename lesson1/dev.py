bc2e41e5e0f6f7463069843528783df3b852fab9
Mikhail <newlevelcenter@gmail.com>
2020-09-04 19:19:33 +0300

    math3

---
+++ lesson1/dev.py
@@ -0,0 +1,22 @@
+import math
+b = 3
+c = 4
+a = b / c
+print(type(a))
+print(a)
+#print(3<4)
+#print(4%2)
+#print(math.sqrt(4))
+#print(math.pow(5,2))
+e = None
+print(e)
+a = "qwe"
+print(type(a)) #<class 'str'>
+a=4
+print(type(a)) #<class 'int'>
+a = 4/3
+print(type(a)) #<class 'float'>
+a = True
+print(type(a)) #<class 'bool'>
+a = None
+print(type(a)) #<class 'NoneType'>
