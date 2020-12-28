8316d04950b366f4a229474bbdfe0f8467364cee
nhladkikh <nikita.gladkikh@explorium.ai>
2020-12-28 10:02:35 +0200

    Fixes hw for module 5

--- lesson05/hw.py
+++ lesson05/hw.py
@@ -1,8 +1,49 @@
 import re
 
 
-CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
-TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u", "f", "h", "ts", "ch", "sh", "sch", "", "y", "e", "u", "ja")
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+TRANSLATION = (
+    "a",
+    "b",
+    "v",
+    "g",
+    "d",
+    "e",
+    "e",
+    "j",
+    "z",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "r",
+    "s",
+    "t",
+    "u",
+    "f",
+    "h",
+    "ts",
+    "ch",
+    "sh",
+    "sch",
+    "",
+    "i",
+    "",
+    "y",
+    "e",
+    "u",
+    "ja",
+    "je",
+    "ji",
+    "g",
+)
+assert len(CYRILLIC_SYMBOLS) == len(
+    TRANSLATION
+), f"{len(CYRILLIC_SYMBOLS)} != {len(TRANSLATION)}"
 TRANS = {}
 for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
     TRANS[ord(c)] = l
@@ -17,4 +58,5 @@ def normalize(name: str) -> str:
 
 if __name__ == "__main__":
     import sys
+
     print(normalize(" ".join(sys.argv[1:])))
